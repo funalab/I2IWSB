8a349e857671bf8d7938a0d1ad9a34da6bd27348
t-morik <morikura.t@gmail.com>
2024-02-25 11:22:58 +0900

    add new cfg for changing fold 4 to 5

--- scripts/diffusion/cross_validation.sh
+++ scripts/diffusion/cross_validation.sh
@@ -4,10 +4,11 @@
 path=$1
 
 # run train
-python src/tools/custom/train.py --conf_file $path/train_fold1.cfg
-python src/tools/diffusion/train.py--conf_file $path/train_fold2.cfg
+python src/tools/diffusion/train.py --conf_file $path/train_fold1.cfg
+python src/tools/diffusion/train.py --conf_file $path/train_fold2.cfg
 python src/tools/diffusion/train.py --conf_file $path/train_fold3.cfg
 python src/tools/diffusion/train.py --conf_file $path/train_fold4.cfg
+python src/tools/diffusion/train.py --conf_file $path/train_fold5.cfg
 
 # run test
 test_result=$(python src/tools/diffusion/test.py --conf_file $path/test.cfg | tee /dev/tty)
--- scripts/gan/cross_validation.sh
+++ scripts/gan/cross_validation.sh
@@ -8,6 +8,7 @@ python src/tools/gan/train.py --conf_file $path/train_fold1.cfg
 python src/tools/gan/train.py --conf_file $path/train_fold2.cfg
 python src/tools/gan/train.py --conf_file $path/train_fold3.cfg
 python src/tools/gan/train.py --conf_file $path/train_fold4.cfg
+python src/tools/gan/train.py --conf_file $path/train_fold5.cfg
 
 # run test
 test_result=$(python src/tools/gan/test.py --conf_file $path/test.cfg | tee /dev/tty)
