e0497d33795aeb5742847e3708b5f90a9a4c23f8
t-morik <morikura.t@gmail.com>
2024-05-20 18:14:38 +0900

    debug

--- src/tools/template/evaluate_outputs.py
+++ src/tools/template/evaluate_outputs.py
@@ -421,7 +421,7 @@ def analyze_dataframe(df, save_dir, file_name):
 
 def evaluate_compare(x, y, savefilepath=None):
     lr = LinearRegression()
-    lr.fit(x.reshape(-1, 1), y.reshape(-1, 1))
+    lr.fit(np.array(x).reshape(-1, 1), np.array(y).reshape(-1, 1))
     y_pred = lr.predict(x)
     mse = mean_squared_error(y_pred=y_pred, y_true=y)
     mae = mean_absolute_error(y_pred=y_pred, y_true=y)
